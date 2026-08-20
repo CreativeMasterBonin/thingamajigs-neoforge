d17896c7ba87ebc9ea27e373898791cf41458b43
blueyMasterMods <17255831+creativemasterbonin@users.noreply.github.com>
2026-08-20 08:06:00 -0400

    1.8.9 finalized features and changes
    Signed-off-by: blueyMasterMods <17255831+CreativeMasterBonin@users.noreply.github.com>

--- src/main/java/net/rk/thingamajigs/block/custom/CurvedMonitor.java
+++ src/main/java/net/rk/thingamajigs/block/custom/CurvedMonitor.java
@@ -3,8 +3,15 @@ package net.rk.thingamajigs.block.custom;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -15,7 +22,10 @@ import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+import net.neoforged.neoforge.common.Tags;
 import net.rk.thingamajigs.blockentity.custom.CurvedMonitorBE;
+import net.rk.thingamajigs.xtras.TCalcStuff;
 import org.jetbrains.annotations.Nullable;
 
 public class CurvedMonitor extends BaseEntityBlock implements SimpleWaterloggedBlock{
@@ -28,6 +38,27 @@ public class CurvedMonitor extends BaseEntityBlock implements SimpleWaterloggedB
                 .noOcclusion());
         this.registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH)
                 .setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if(level.isClientSide()){
+            if(stack.is(Tags.Items.TOOLS_WRENCH) || stack.is(Tags.Items.TOOLS_SHEAR)){
+                player.playSound(SoundEvents.CALCITE_HIT,0.75f, TCalcStuff.nextFloatBetweenInclusive(0.97f,1.1f));
+                return ItemInteractionResult.SUCCESS;
+            }
+        }
+        else{
+            if(stack.is(Tags.Items.TOOLS_WRENCH) || stack.is(Tags.Items.TOOLS_SHEAR)){
+                CurvedMonitorBE curvedMonitor = (CurvedMonitorBE)level.getBlockEntity(pos);
+                if(curvedMonitor instanceof CurvedMonitorBE){
+                    curvedMonitor.hideKeyboard = !curvedMonitor.hideKeyboard;
+                    curvedMonitor.updateBlock();
+                }
+                return ItemInteractionResult.SUCCESS;
+            }
+        }
+        return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
     }
 
     @Override
