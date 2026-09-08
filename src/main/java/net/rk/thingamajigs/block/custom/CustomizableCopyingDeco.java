2b6fd003db74ce137a4c721386814b4de23efc84
blueyMasterMods <17255831+creativemasterbonin@users.noreply.github.com>
2026-09-08 15:11:50 -0400

    1.9.1 update

--- src/main/java/net/rk/thingamajigs/block/custom/CustomizableCopyingDeco.java
+++ src/main/java/net/rk/thingamajigs/block/custom/CustomizableCopyingDeco.java
@@ -11,13 +11,17 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.*;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
@@ -31,16 +35,18 @@ import net.rk.thingamajigs.xtras.TCalcStuff;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class CustomizableCopyingDeco extends BaseEntityBlock {
+public class CustomizableCopyingDeco extends BaseEntityBlock implements SimpleWaterloggedBlock {
     public static final ModelProperty<BlockState> BLOCKSTATE_ID = new ModelProperty<>();
     public static final MapCodec<CustomizableCopyingDeco> CODEC = simpleCodec(CustomizableCopyingDeco::new);
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public CustomizableCopyingDeco(Properties p) {
         super(p.noOcclusion().pushReaction(PushReaction.BLOCK).instrument(NoteBlockInstrument.BIT));
+        this.registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED,false));
     }
 
     @Override
-    protected MapCodec<? extends BaseEntityBlock> codec() {
+    public MapCodec<? extends BaseEntityBlock> codec() {
         return CODEC;
     }
 
@@ -157,6 +163,23 @@ public class CustomizableCopyingDeco extends BaseEntityBlock {
             }
         }
         return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+    }
+
+    @Override
+    public void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState bs) {
+        return bs.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(bs);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
+        return this.defaultBlockState().setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
     }
 
     @Override
---
+++ src/main/java/net/rk/thingamajigs/mix/TChunkRenderTypes.java
@@ -0,0 +1,21 @@
+package net.rk.thingamajigs.mix;
+
+/*@Debug(export = true)
+@Mixin(value = ItemBlockRenderTypes.class)*/
+public class TChunkRenderTypes {
+    /*@Shadow private static boolean renderCutout;
+
+    @Inject(method = "getChunkRenderType",at={@At(value = "RETURN")},cancellable = true)
+    private static void getChunkRenderTypeThingamajigs(BlockState state, CallbackInfoReturnable<RenderType> cir){
+        if(state.getBlock() instanceof CustomLeavesBlock){
+            cir.setReturnValue(renderCutout ? RenderType.cutoutMipped() : RenderType.solid());
+        }
+    }
+
+    @Inject(method = "getMovingBlockRenderType",at={@At(value = "RETURN")},cancellable = true)
+    private static void getMovingBlockRenderTypeThingamajigs(BlockState state, CallbackInfoReturnable<RenderType> cir){
+        if(state.getBlock() instanceof CustomLeavesBlock){
+            cir.setReturnValue(renderCutout ? RenderType.cutoutMipped() : RenderType.solid());
+        }
+    }*/
+}
